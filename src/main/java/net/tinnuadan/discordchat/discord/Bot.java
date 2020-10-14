b497a2f3e189aaf2a5970021781777fb6c302ce2
tinnuadan <tinnuadan@gmail.com>
2020-10-14 14:59:34 +0200

    bot stub

--- src/main/java/net/tinnuadan/discordchat/discord/Bot.java
+++ src/main/java/net/tinnuadan/discordchat/discord/Bot.java
@@ -1,5 +1,17 @@
 package net.tinnuadan.discordchat.discord;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+
 public class Bot
 {
+  private final JDA jda;
+
+  public Bot(String token) throws LoginException {
+    jda = JDABuilder.createDefault(token).build();
+  }
+
+
 }

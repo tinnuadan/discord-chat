b6ea3ef234c2c30ba34600ba74bcffc8dff78982
tinnuadan <tinnuadan@gmail.com>
2020-10-15 13:59:46 +0200

    Send game message to discord

--- src/main/java/net/tinnuadan/discordchat/discord/Bot.java
+++ src/main/java/net/tinnuadan/discordchat/discord/Bot.java
@@ -2,16 +2,21 @@ package net.tinnuadan.discordchat.discord;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.requests.restaction.MessageAction;
 import net.tinnuadan.discordchat.DiscordChatMod;
 import net.tinnuadan.discordchat.common.IMessageSender;
 
 import javax.security.auth.login.LoginException;
+import java.util.Objects;
 
 public class Bot
 {
   private final JDA jda;
+  private final String channelID;
 
   public Bot(String token, String channelID, IMessageSender mcMessageSender) throws LoginException {
+    this.channelID = channelID;
     jda = JDABuilder.createDefault(token)
       .addEventListeners(new MessageListener(channelID, mcMessageSender))
       .build();
@@ -21,6 +26,9 @@ public class Bot
   {
     DiscordChatMod.LOGGER.info("Sending message to discord");
     DiscordChatMod.LOGGER.info(message);
-    //todo: send to discord
+    
+    TextChannel channel = jda.getTextChannelById(channelID);
+    assert channel != null;
+    channel.sendMessage(message).queue();
   }
 }
